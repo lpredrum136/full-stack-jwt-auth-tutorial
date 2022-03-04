dfac98bc3ef27cae56718763db6de9acf1866185
Henry Nguyen <nguyenhai1591@gmail.com>
2022-03-04 21:09:59 +1000

    getting refresh token

--- client/src/utils/jwt.ts
+++ client/src/utils/jwt.ts
@@ -1,10 +1,27 @@
+import jwtDecode, { JwtPayload } from 'jwt-decode'
+
 const JWTManager = () => {
 	let inMemoryToken: string | null = null
+	let refreshTokenTimeoutId: number | null = null
 
 	const getToken = () => inMemoryToken
 
 	const setToken = (accessToken: string) => {
 		inMemoryToken = accessToken
+
+		// Decode and set countdown to refresh
+		const decoded = jwtDecode<JwtPayload & { userId: number }>(accessToken)
+
+		setRefreshTokenTimeout((decoded.exp as number) - (decoded.iat as number))
+		return true
+	}
+
+	const setRefreshTokenTimeout = (delay: number) => {
+		// 5s before token expires
+		refreshTokenTimeoutId = window.setTimeout(
+			getRefreshToken,
+			delay * 1000 - 5000
+		)
 	}
 
 	return { getToken, setToken }
